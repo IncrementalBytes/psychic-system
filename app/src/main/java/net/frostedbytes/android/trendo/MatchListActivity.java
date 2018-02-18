cecb5f53a59d9b4716ef54882087af61f8b39484
Ryan W <befuddledgeek@gmail.com>
2018-02-17 22:21:10 -0800

    Added image assets for preparation to rollout app; cleaned up some inspection issues

--- app/src/main/java/net/frostedbytes/android/trendo/MatchListActivity.java
+++ app/src/main/java/net/frostedbytes/android/trendo/MatchListActivity.java
@@ -2,7 +2,6 @@ package net.frostedbytes.android.trendo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,8 +13,6 @@ import android.view.MenuItem;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
